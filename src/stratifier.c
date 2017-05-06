789023b1b4f4dbed4d088e332ad391db046769da
Con Kolivas <kernel@kolivas.org>
2017-05-06 22:40:33 +1000

    Update time after processing transactions which can be delayed.

--- src/stratifier.c
+++ src/stratifier.c
@@ -1752,7 +1752,6 @@ static void block_update(ckpool_t *ckp, int *prio)
 	bool ret = false;
 	txntable_t *txns;
 	workbase_t *wb;
-	time_t now_t;
 
 retry:
 	wb = generator_getbase(ckp);
@@ -1803,12 +1802,6 @@ retry:
 	generate_coinbase(ckp, wb);
 
 	add_base(ckp, sdata, wb, &new_block);
-	/* Reset the update time to avoid stacked low priority notifies. Bring
-	 * forward the next notify in case of a new block. */
-	now_t = time(NULL);
-	if (new_block)
-		now_t -= ckp->update_interval / 2;
-	sdata->update_time = now_t;
 
 	stratum_broadcast_update(sdata, wb, new_block);
 	ret = true;
@@ -1825,7 +1818,13 @@ retry:
 	 * propagation. */
 	if (likely(txns))
 		update_txns(ckp, sdata, txns, true);
+	/* Reset the update time to avoid stacked low priority notifies. Bring
+	 * forward the next notify in case of a new block. */
+	sdata->update_time = time(NULL);
+	if (new_block)
+		sdata->update_time -= ckp->update_interval / 2;
 out:
+
 	cksem_post(&sdata->update_sem);
 
 	/* Send a ping to miners if we fail to get a base to keep them
