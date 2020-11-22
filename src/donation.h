ffe98d7bb245d5e9166c869bff92c159ca3cf0a1
Calin Culianu <calin.culianu@gmail.com>
2020-11-22 05:40:38 +0200

    Updated a comment in donation.h

--- src/donation.h
+++ src/donation.h
@@ -13,7 +13,9 @@
 /* Some constants related to auto-donation. Donations go as the 2nd and 3rd
    outputs of the coinbase generation tx, and are subtracted from pool fee. */
 
-#define DONATION_FRACTION 10 // 10% of pool_fee per address below (set to 0 if you wish to disable donation)
+// 10% of pool_fee per address below (set to 0 if you wish to disable donation,
+// or specify "disable_dev_donation": true in the conf file as a top-level key.
+#define DONATION_FRACTION 10
 
 #define DONATION_ADDRESS_CALIN "1Ca1inCimwRhhcpFX84TPRrPQSryTgKW6N" // Calin (dev)
 #define DONATION_ADDRESS_BCHN "3NoBpEBHZq6YqwUBdPAMW41w5BTJSC7yuQ"  // BCHN donation wallet
